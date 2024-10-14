69703d28d969658a4b7c7c8f5949d17a7c7d4901
JaySmithTech <70680292+jaysmithtech@users.noreply.github.com>
2024-10-14 00:03:34 -0500

    updates

--- main.go
+++ main.go
@@ -7,9 +7,40 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+type Todo struct {
+	ID        int    `json:"id"`
+	Completed bool   `json:"completed"`
+	Body      string `json:"body"`
+}
+
 func main() {
 	fmt.Println("Hello, World!")
 	app := fiber.New()
+
+	todos := []Todo{}
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(fiber.Map{"msg": "Hello, World!"})
+	})
+
+	app.Post("/api/todos", func(c *fiber.Ctx) error {
+		todo := &Todo{}
+
+		if err := c.BodyParser(todo); err != nil {
+			return err
+		}
+
+		if todo.Body == "" {
+			return c.Status(400).JSON(fiber.Map{"error": "Todo body is required"}) // Parse the body of the request
+
+		}
+
+		todo.ID = len(todos) + 1
+		todos = append(todos, *todo)
+
+		return c.Status(201).JSON(todo)
+
+	})
 
 	log.Fatal(app.Listen(":4000"))
 
